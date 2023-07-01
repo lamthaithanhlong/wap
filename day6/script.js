414bb703ead94b3aa74dc9ccfe0d5b6d12dde556
Lam Thai Thanh Long <lamthaithanhlong@gmail.com>
2023-07-01 10:46:41 -0500

    update demo code day 6

--- day6/script.js
+++ day6/script.js
@@ -1,3 +1,7 @@
 console.log(location.search)
 const q = new URLSearchParams(location.search)
-console.log(q.get('name'), q.get('value'))
+console.log("GET result of search bar " + q.get('name'), q.get('value'))
+
+function goBack() {
+    history.back();
+}
